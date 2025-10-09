3bf9c8d9a76618de5bcddd95803543ce30f90939
yl <7127607+U0001F3A2@users.noreply.github.com>
2025-10-09 17:05:32 +0900

    second demo

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from fastapi import Request
-
 from dotenv import load_dotenv
 import os
+import httpx
 
 from nitro_toolkit.enclave import BaseNitroEnclaveApp
 from nitro_toolkit.util.log import logger
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-print(OPENAI_API_KEY)
 
 class App(BaseNitroEnclaveApp):
 
@@ -20,6 +19,7 @@ class App(BaseNitroEnclaveApp):
     def add_endpoints(self):
         self.app.add_api_route("/add_two", self.add_two, methods=["POST"])
         self.app.add_api_route("/hello_world", self.hello_world, methods=["GET"])
+        self.app.add_api_route("/chat", self.chat, methods=["POST"])
     
     async def add_two(self, request: Request):
         body = await request.json()
@@ -29,6 +29,27 @@ class App(BaseNitroEnclaveApp):
 
     async def hello_world(self, request: Request):
         return self.response("Hello World")
+
+    async def chat(self, request: Request):
+        body = await request.json()
+        prompt = body.get("prompt")
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "https://api.openai.com/v1/chat/completions",
+                headers={
+                    "Authorization": f"Bearer {OPENAI_API_KEY}",
+                    "Content-Type": "application/json"
+                },
+                json={
+                    "model": "gpt-4o-mini",
+                    "messages": [{"role": "user", "content": prompt}]
+                }
+            )
+        logger.info(response.json())
+            
+        return self.response(response.json()["choices"][0]["message"]["content"])
+        
 
 
 if __name__ == "__main__":
